d30d2a84f60df9e43644665a3901b86c1afacb47
Tatiana Shestopalova <missis.dreamka@yandex.ru>
2022-01-04 15:26:32 +0300

    Реализован JAMES.

--- test/test_create_project.py
+++ test/test_create_project.py
@@ -4,11 +4,11 @@ from model.project import Project
 def test_create_project(app, orm):
     app.session.login("administrator", "root")
     old_projects = orm.get_projects_list()
-    new_project = Project(project_name="project_name",
+    new_project = Project(project_name="project_name-2",
                           status="release",
                           inherit_global=True,
                           view_status="private",
-                          description="description")
+                          description="description-2")
     app.project.create(new_project)
     new_projects = orm.get_projects_list()
     assert len(old_projects) + 1 == len(new_projects)
