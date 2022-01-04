7aeb12af8abcdf56575361c778e8712219eb98bd
Tatiana Shestopalova <missis.dreamka@yandex.ru>
2022-01-04 16:22:21 +0300

    Реализовано получение почты по протоколу POP3.

--- test/test_create_project.py
+++ test/test_create_project.py
@@ -1,14 +1,21 @@
+import string
+import random
 from model.project import Project
+
+
+def random_project_name(prefix, maxlen):
+    symbols = string.ascii_letters
+    return prefix + ''.join([random.choice(symbols) for _ in range(random.randrange(maxlen))])
 
 
 def test_create_project(app, orm):
     app.session.login("administrator", "root")
     old_projects = orm.get_projects_list()
-    new_project = Project(project_name="project_name-2",
+    new_project = Project(project_name=random_project_name("project_name_", 10),
                           status="release",
                           inherit_global=True,
                           view_status="private",
-                          description="description-2")
+                          description="description")
     app.project.create(new_project)
     new_projects = orm.get_projects_list()
     assert len(old_projects) + 1 == len(new_projects)
